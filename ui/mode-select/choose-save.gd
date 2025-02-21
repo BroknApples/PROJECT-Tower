5c2ffc0575501961d2c3ca0deabd3b72f81f5e35
Brokn_Apples <trentons708@gmail.com>
2025-02-20 18:22:29 -0600

    Merge versions between computers

--- ui/mode-select/choose-save.gd
+++ ui/mode-select/choose-save.gd
@@ -2,4 +2,4 @@ extends Control
 
 # Start Game button pressed -- TOOD: Change to load a save file once implemented
 func _on_start_game_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/World1.tscn")
+	get_tree().change_scene_to_file("res://levels/World1.tscn")
